97ac5463a1d58ecc67486d8ca07776fc8263ac01
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-11-02 12:08:00 +0700

    Pertemuan 3

--- app/src/main/java/com/example/quizio/Soal1Activity.java
+++ app/src/main/java/com/example/quizio/Soal1Activity.java
@@ -2,16 +2,24 @@ package com.example.quizio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class Soal1Activity extends AppCompatActivity {
 
+    private TextView textViewJawabanA;
+    private TextView textViewJawabanB;
+    private TextView textViewJawabanC;
+    private TextView textViewJawabanD;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soal1);
+
+        textViewJawabanA = findViewById(R.id.text_view_jawaban_a_soal_1);
+        textViewJawabanB = findViewById(R.id.text_view_jawaban_b_soal_1);
+        textViewJawabanC = findViewById(R.id.text_view_jawaban_c_soal_1);
+        textViewJawabanD = findViewById(R.id.text_view_jawaban_d_soal_1);
     }
 }
